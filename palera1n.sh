b5d82d5c7da437e902c8978e7ce9f981d0d51bf8
Mineek <mineek@mineek.nl>
2022-12-27 13:57:05 +0100

    ...

--- palera1n.sh
+++ palera1n.sh
@@ -613,13 +613,6 @@ if [ ! -f boot-"$deviceid"/ibot.img4 ]; then
             "$dir"/iBootpatch2 --t8015 ibot.patched ibot.patched2
         fi
 
-        if [ "$os" = 'Linux' ]; then
-            sed -i 's/\/\kernelcache/\/\kernelcachd/g' ibot.patched2
-        else
-            LC_ALL=C sed -i.bak -e 's/s\/\kernelcache/s\/\kernelcachd/g' ibot.patched2
-            rm *.bak
-        fi
-
         cd ..
         "$dir"/img4 -i work/ibot.patched2 -o boot-"$deviceid"/ibot.img4 -M blobs/"$deviceid"-"$version".der -A -T ibss
 
